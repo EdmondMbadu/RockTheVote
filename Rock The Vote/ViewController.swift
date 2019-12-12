b1ea3b0030a797071e02a037737928959fd5a89f
Edmond Mbadu <mbadungoma@gmail.com>
2019-12-12 08:03:03 -0500

    Initial Commit

---
+++ Rock The Vote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock The Vote
+//
+//  Created by Mbadu, Edmond Ngoma on 12/12/19.
+//  Copyright © 2019 Mbadu, Edmond Ngoma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
